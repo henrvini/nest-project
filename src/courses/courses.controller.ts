408fddc5ec0a65bcba6e5b1c026e6f8a747c0ac3
Vinicius Rodrigues <vini0593@gmail.com>
2022-07-28 16:39:08 -0300

    created methods and entities for courses

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -9,9 +9,12 @@ import {
   Post,
   Res,
 } from '@nestjs/common';
+import { CoursesService } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
+  constructor(private readonly coursesService: CoursesService) {}
+
   @Get()
   findAll(@Res() response) {
     return response.status(200).send(`Listagem de cursos`);
