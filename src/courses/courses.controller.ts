56a34d02cba1ab04257c5da2d2f1b7a5de058466
Vinicius Rodrigues <vini0593@gmail.com>
2022-07-28 15:15:52 -0300

    created course controller

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
-  @Get('list')
-  findAll() {
-    return 'Listagem de cursos';
+  @Get()
+  findAll(@Res() response) {
+    return response.status(200).send(`Listagem de cursos`);
+  }
+
+  @Get(':id')
+  findOne(@Param() params) {
+    return `Curso #${params.id}`;
+  }
+
+  @Post()
+  @HttpCode(204)
+  create(@Body() body) {
+    return body;
   }
 }
